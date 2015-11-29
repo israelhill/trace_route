4b62114cd3bfc45d59130831e7ce00ea07d30e98
Israel Hill <idh@case.edu>
2015-11-28 23:21:11 -0500

    switched to single probe. Trying to print the data I get back

--- distMeasurement.py
+++ distMeasurement.py
@@ -9,45 +9,42 @@ def main(destination):
     icmp = socket.getprotobyname('icmp')
     udp = socket.getprotobyname('udp')
     # Start the time to live at 1
-    ttl = 1
-    max_hops = 30
+    ttl = 32
+    max_hops = 128
 
-    while True:
-        recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
-        send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, udp)
-        send_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
 
-        # bind the adress to recv_socket. empty string because we are accepting
-        # packets from any host on port 33434 (unlikely port)
-        recv_socket.bind(("", port))
 
-        # Send no data (empty string) to the destination host
-        # on an unlikely port
-        send_socket.sendto("", (destination, port))
+    recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
+    send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, udp)
+    send_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
 
-        rcvd_packet, current_address = None
-        try:
-            # get data from the recv_socket,
-            # recvfrom() returns the packet data and adress
-            rcvd_packet, current_address = recv_socket.recvfrom(1500)
+    # bind the adress to recv_socket. empty string because we are accepting
+    # packets from any host on port 33434 (unlikely port)
+    recv_socket.bind(("", port))
 
-            # get the IP address
-            current_address = current_address[0]
-            try:
-                # reverse DNS lookup
-                current_name = socket.gethostbyaddr(current_address)[0]
-            except socket.error:
-                current_name = current_address
-        except socket.error:
-            pass
-        finally:
-            send_socket.close()
-            recv_socket.close()
+    # Send no data (empty string) to the destination host
+    # on an unlikely port
+    send_socket.sendto("", (destination, port))
 
-        ttl += 1
-        # break out the loop when the following are true:
-        if current_address == dest_address or ttl > max_hops:
-            break
+    rcvd_packet, current_address = None
+    try:
+        # get data from the recv_socket,
+        # recvfrom() returns the packet data and adress
+        rcvd_packet, current_address = recv_socket.recvfrom(1500)
+
+        # get the IP address
+        current_address = current_address[0]
+
+        print "Received Packet: " + rcvd_packet
+    except socket.error:
+        pass
+    finally:
+        send_socket.close()
+        recv_socket.close()
+
+    # break out the loop when the following are true:
+    if current_address == dest_address or ttl > max_hops:
+        break
 
 if __name__ == '__main__':
     main('google.com')
