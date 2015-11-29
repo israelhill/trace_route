3d230836f65dbd611248e61535bd57605d3d64e4
Israel Hill <idh@case.edu>
2015-11-28 23:44:10 -0500

    testing select with a timeout of 3 seonds

--- distMeasurement.py
+++ distMeasurement.py
@@ -26,7 +26,7 @@ def main(destination):
     # Send no data (empty string) to the destination host
     # on an unlikely port
     send_socket.sendto("", (destination, port))
-    ready = select.select([recv_socket], [], [])
+    ready = select.select([recv_socket], [], [], 3.0)
 
     rcvd_packet, current_address = None
     try:
